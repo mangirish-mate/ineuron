8fc4812f903bbae269dc287957b5a715917b4829
mangirish-mate <mangirish.mate@gmail.com>
2022-08-15 21:20:01 +0530

    #Codes from MySQl to Visualisation push to github

--- mongotest.py
+++ mongotest.py
@@ -8,6 +8,19 @@ d = {
     "email":"sudhanshu@ineuron.ai",
     "surname":"kumar"
     }
+
+d = {
+    "name": "sudhanshu",
+    "email":"sudhanshu@ineuron.ai",
+    "surname":"kumar"
+    }
+
+d = {
+    "name": "sudhanshu",
+    "email":"sudhanshu@ineuron.ai",
+    "surname":"kumar"
+    }
+
 db1 = client['monogotest']
 coll = db1['test']
 coll.insert_one(d)
